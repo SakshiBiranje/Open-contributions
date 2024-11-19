bf5b71d3e4f5e2a15f259d12e3a2e1f88d12b88c
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-11-19 05:56:15 +0530

    Loops in java

---
+++ SAKSHI BIRAJE'S CONTRIBUTIONS/JAVA CODE/loops.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class loops{
+    
+    public static void main(String args[]) {
+      //for loop
+  //for(int i=0;i<4;i++){
+  //System.out.println("Hello World");
+  //}
+
+      //print num from 0 to 10
+    //for(int i=0;i<11;i++){
+    //System.out.println(i);
+    //}
+
+    //while loop
+   // int i=0;
+    //while(i<11){
+      //System.out.println(i);
+      //i=i+1;
+   // }
+
+   //do while loop
+    //int i=0;
+    //do {
+      //System.out.println(i);
+      //i = i + 1 ;
+    //} while(i<11);
+  //}
+
+  //sum of n natural numbers
+       // Scanner sc = new Scanner(System.in);
+       // int n =sc.nextInt();
+
+       // int sum=0;
+       // for (int i=0;i<=n;i++){
+         // sum = sum + i;
+
+        //}
+       // System.out.println(sum);
+ 
+
+       //print table of a num input by user
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<11;i++){
+          System.out.println(i*n);
+
+        }
+          }
+        }
+          
+    
+    
+    
+
+
+   
